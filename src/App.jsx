ea846f8c8c3ceb58e428664b668609d19c9d14b9
Uqyanzie Bintang <uqyanzie@gmail.com>
2023-11-20 21:26:58 +0700

    Add Beker Wms Project Summary

--- src/App.jsx
+++ src/App.jsx
@@ -117,7 +117,7 @@ export default function App() {
                           Beker WMS
                         </h5>  
                       </div>
-                      <p className="font-inter">Beker WMS is a workshop management app, built for HBBA (Himpunan Bengkel Binaan Astra) organization</p>
+                      <p className="font-inter">Beker WMS is a workshop management app, built for HBBA (Himpunan Bengkel Binaan Astra) organization. It manages the workshop master data, customer data, customer vehicle data, provides cashier system & invoice printing, and also manages service execution processing and reporting.</p>
                       <div className="flex flex-wrap pt-3 gap-3">
                         <div className="rounded-full border border-blue-500 text-xs p-1 px-2">
                           HTML5
@@ -153,7 +153,7 @@ export default function App() {
                         </h5>  
                         <svg className="text-sm mt-1 hover:text-blue-500" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path d="M320 0c-17.7 0-32 14.3-32 32s14.3 32 32 32h82.7L201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L448 109.3V192c0 17.7 14.3 32 32 32s32-14.3 32-32V32c0-17.7-14.3-32-32-32H320zM80 32C35.8 32 0 67.8 0 112V432c0 44.2 35.8 80 80 80H400c44.2 0 80-35.8 80-80V320c0-17.7-14.3-32-32-32s-32 14.3-32 32V432c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16H192c17.7 0 32-14.3 32-32s-14.3-32-32-32H80z"/></svg>
                       </div>
-                      <p className="font-inter">Agpot is an IoT-based system development project that utilizes temperature, humidity and rainfall recording devices with solar panel energy sources that are integrated with internet servers and can be monitored through web pages.</p>
+                      <p className="font-inter">Agpot is an IoT-based application that utilizes temperature, humidity and rainfall recording devices with solar panel energy sources that are integrated with internet servers and can be monitored through web pages.</p>
                       <div className="flex flex-wrap pt-3 gap-3">
                         <div className="rounded-full border border-blue-500 text-xs p-1 px-2">
                           HTML5
